a3bd95c263ee4c915d4b690631c7b305dc406126
Francis Hwang <francis@diversionmedia.com>
2008-05-29 13:25:45 -0400

    extracted auto_cron template installation to a generator

--- install.rb
+++ install.rb
@@ -4,4 +4,4 @@ require 'config/environment'
 require 'rails_generator'
 require 'rails_generator/scripts/generate'
 
-Rails::Generator::Scripts::Generate.new.run(%w(auto_cron))
+Rails::Generator::Scripts::Generate.new.run %w(auto_cron)
